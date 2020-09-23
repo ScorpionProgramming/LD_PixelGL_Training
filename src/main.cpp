83b7293d2e0121c1900d1468db6522acf2bf0d3c
Scorpion A <scorpionprogramming@gmail.com>
2020-09-23 18:44:31 +0200

    Pixel draw Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,38 @@
 #include <GLFW/glfw3.h>
+#include <stdlib.h>
+
+void update(unsigned char* data, unsigned int& width, unsigned int& height){
+    unsigned int i;
+    for(i = 0; i < width * height; i++){
+        data[i * 3 + 0] = (char)(rand() % 256);
+        data[i * 3 + 1] = (char)(rand() % 256);
+        data[i * 3 + 2] = (char)(rand() % 256);
+    }
+}
 
 int main(void)
 {
     GLFWwindow* window;
+
+    unsigned int width  = 640;
+    unsigned int height = 480; 
+    //^^^ for later generate width and height with number of pixels and pixels size
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+
+    //If everything works generate data
+    unsigned char* data = new unsigned char[width * height * 3];
+    
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -24,7 +42,8 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
+        update(data, width, height);
+        glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
